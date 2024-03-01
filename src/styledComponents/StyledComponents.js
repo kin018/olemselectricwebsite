8f22a75f432e38995f0b0e3a8ae12957a035f7c7
kin018 <60412598+kin018@users.noreply.github.com>
2024-03-01 15:39:19 -0500

    Created Navbar Componet as well as initial theme colors

---
+++ src/styledComponents/StyledComponents.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+// Theme Definitions
+export const darkTheme = {
+  bg: '#1C1C27',
+  bgLight: '#1C1E27',
+  primary: '#854CE6',
+  text_primary: '#F2F3F4',
+  text_secondary: '#b1b2b3',
+  card: '#171721',
+  card_light: 'rgba(25, 25, 36,1)',
+  button: '#854CE6',
+  white: '#FFFFFF',
+  black: '#000000',
+  input: '#171721' + 99,
+  border: '#644ed7',
+  buttonHover: '#854CE6',
+};
+
+export const lightTheme = {
+  bg: '#F8F8FF',
+  bgLight: '#f0f0f0',
+  primary: '#be1adb',
+  text_primary: '#111111',
+  text_secondary: 'black',
+  card: '#FFFFFF',
+  card_light: 'rgba(255, 255, 255, 1)',
+  button: '#5c5b5b',
+  white: '#FFFFFF',
+  black: '#000000',
+  input: '#e2e8f0',
+  border: '#cad4e0',
+  buttonHover: '#854CE6',
+};
